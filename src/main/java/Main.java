2bb365a5ee496249d16d88cacdde986fb56dcde1
sniang1 <96024696+sniang1@users.noreply.github.com>
2022-04-19 15:21:40 -0400

    firt try morgage calc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,13 @@
-public class HelloWorld {
+import java.text.NumberFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        double p = 100000;
+        double r = 0.005;
+        double n = 180;
+        double m = p * ((r* Math.pow((1+r), n))/ (Math.pow((1+r), n) - 1));
+        //NumberFormat result = currency.format
+        System.out.println("your morgage is " + m);
+    }
     
 }
