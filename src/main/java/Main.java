91b64ebdf5077bdb2a0eef7b52ccc4bebcc551d4
sniang1 <96024696+sniang1@users.noreply.github.com>
2022-04-20 15:35:01 -0400

    complete mortgage calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,50 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        int principal = 0;
+        float monthlyInterest = 0;
+        float annualInterest = 0;
+        byte years = 0;
+        int numberOfPayments = 0;
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("please enter the principal");
-        double p = input.nextDouble();
-        System.out.println("please enter rate");
-        double r = input.nextDouble();
-        System.out.println("please enter the number of months");
-        double n = input.nextDouble();
-        double m = p * ((r* Math.pow((1+r), n))/ (Math.pow((1+r), n) - 1));
-        int mortgage = (int) m;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Principal");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("error : enter a number between 1000 and 1.000.000");
+        }
+
+        while (true) {
+            System.out.println("Annual Interest Rate");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println(" error: please enter a number between 1 and 30");
+        }
+
+        while (true) {
+            System.out.println(" Period in (Years)");
+            years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("error: please enter a number between 1 and 12");
+        }
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow((1 + monthlyInterest), numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments));
         String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("your mortgage is $" + mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+
     }
-    
 }
+
